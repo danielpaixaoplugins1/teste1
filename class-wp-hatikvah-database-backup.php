d7f9413369253f7956d94690f0f07bebd173b8d6
danielpaixaoplugins1 <158494169+danielpaixaoplugins1@users.noreply.github.com>
2024-02-13 20:33:17 -0400

    Create class-wp-hatikvah-database-backup.php

---
+++ class-wp-hatikvah-database-backup.php
@@ -0,0 +1,8 @@
+<?php
+if ( ! class_exists( 'WP_Hatikvah_Database_Backup' ) ) {
+    class WP_Hatikvah_Database_Backup {
+        public function __construct() {
+            // Inicialização e hooks relacionados ao backup
+        }
+    }
+}
