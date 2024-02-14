e5e64a41d6082ba4ea6d1c56bc2ef37d35664680
danielpaixaoplugins1 <158494169+danielpaixaoplugins1@users.noreply.github.com>
2024-02-13 20:35:56 -0400

    Create database-optimize.php

---
+++ database-optimize.php
@@ -0,0 +1,8 @@
+<?php
+if ( ! class_exists( 'WP_Hatikvah_Database_Optimize' ) ) {
+    class WP_Hatikvah_Database_Optimize {
+        public function __construct() {
+            // Inicialização e hooks relacionados à otimização
+        }
+    }
+}
