317f80d0741b2bc9250c2079842b59349aa1e7ae
danielpaixaoplugins1 <158494169+danielpaixaoplugins1@users.noreply.github.com>
2024-02-13 20:35:02 -0400

    Create class-wp-hatikvah-database-cleanup.php

---
+++ class-wp-hatikvah-database-cleanup.php
@@ -0,0 +1,8 @@
+<?php
+if ( ! class_exists( 'WP_Hatikvah_Database_Cleanup' ) ) {
+    class WP_Hatikvah_Database_Cleanup {
+        public function __construct() {
+            // Inicialização e hooks relacionados à limpeza
+        }
+    }
+}
